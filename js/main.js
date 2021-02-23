99ce926773eff590560113a27217abdf596b559a
a.merkulov <merkuller26@gmail.com>
2021-02-23 23:08:09 +0500

    1. Страница Кто мы; - (готово) 2. Страница поиска; - (готово) 3. Доделать карточки с попапами; - (основное готово)4. Убрать фон картинки у карточки; - (готово) 5. Сделать прокрутку содержимого слайда и убрать форму яйца иконки. Все слайды сделать одной высоты; - (готово) 6. Поправить страницу Корзины - выровнить по хлебным крошкам.; - (готово)

--- js/main.js
+++ js/main.js
@@ -1063,6 +1063,39 @@ if (document.querySelector('.popover_productCard')) {
     }
 }
 
+if (document.querySelector('.productCard')) {
+    let popup = document.querySelector('.popup');
+    let cards = document.querySelectorAll('.productCard');
+    let closePopupBtn = popup.querySelector('.popupCloseBtn');
+    let body = document.querySelector('body');
+    let popupProductName = popup.querySelector('.popupProductName');
+    let hiddenFormField = popup.querySelector('.goodName');
+
+    let cardClickHandler = function(item) {
+        let productCardBtn = item.querySelector('.refineRemainder');
+        productCardBtn.addEventListener('click', function(evt) {
+            popup.classList.add('showPopup');
+            let prodcutName = item.querySelector('.productDescription > a');
+            popupProductName.textContent = prodcutName.textContent;
+            hiddenFormField.value = prodcutName.textContent;
+            let popupActive = document.querySelector('.popup.showPopup');
+        });
+    }
+
+    for (let i = 0; i < cards.length; i ++) {
+        cardClickHandler(cards[i]);
+    };
+
+    closePopupBtn.addEventListener('click', function() {
+        popup.classList.remove('showPopup');
+    });
+
+    document.addEventListener('keydown', function(evt){
+        if (evt.key === 'Escape') {
+            popup.classList.remove('showPopup');
+        }
+    });
+}
 // Этот скрипт отображает баннер страницы
 if (document.querySelector('.slider_osbOnFloor')) {
     $('.slider_osbOnFloor').slick({
---
+++ script/showPopup.js
@@ -0,0 +1,33 @@
+if (document.querySelector('.productCard')) {
+    let popup = document.querySelector('.popup');
+    let cards = document.querySelectorAll('.productCard');
+    let closePopupBtn = popup.querySelector('.popupCloseBtn');
+    let body = document.querySelector('body');
+    let popupProductName = popup.querySelector('.popupProductName');
+    let hiddenFormField = popup.querySelector('.goodName');
+
+    let cardClickHandler = function(item) {
+        let productCardBtn = item.querySelector('.refineRemainder');
+        productCardBtn.addEventListener('click', function(evt) {
+            popup.classList.add('showPopup');
+            let prodcutName = item.querySelector('.productDescription > a');
+            popupProductName.textContent = prodcutName.textContent;
+            hiddenFormField.value = prodcutName.textContent;
+            let popupActive = document.querySelector('.popup.showPopup');
+        });
+    }
+
+    for (let i = 0; i < cards.length; i ++) {
+        cardClickHandler(cards[i]);
+    };
+
+    closePopupBtn.addEventListener('click', function() {
+        popup.classList.remove('showPopup');
+    });
+
+    document.addEventListener('keydown', function(evt){
+        if (evt.key === 'Escape') {
+            popup.classList.remove('showPopup');
+        }
+    });
+}
