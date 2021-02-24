d5bacc489098e341e89f9da25edb29d926fbb36d
a.merkulov <merkuller26@gmail.com>
2021-02-24 10:49:29 +0500

    Appear of footer list elements

--- script/footerListElements_index.js
+++ script/footerListElements_index.js
@@ -1,25 +1,25 @@
 // Этот скрипт определяет количество дополнительных элементов списка и по нажатию на кнопку "ещё" показывает их.
 
-let footerList_index = document.querySelector('.footerMenuByApplicationList'); 
-let elementsList_index = footerList_index.querySelectorAll('li');
-let valueHiddenElements_index = footerList_index.querySelector('.hiddenListElements_index');
-let moreListElements_index = footerList_index.querySelector('.moreEl');
+let footerList = document.querySelector('.footerMenuByApplicationList'); 
+let elementsList = footerList.querySelectorAll('li');
+let valueHiddenElements = footerList.querySelector('.hiddenListElements');
+let moreListElements = footerList.querySelector('.moreEl');
 let counter = 0;
-let hiddenListElements_index = [];
+let hiddenListElements = [];
 
 
-for (let i = 0; i <= elementsList_index.length - 1; i ++) {
+for (let i = 0; i <= elementsList.length - 1; i ++) {
     counter ++;
     if (counter > 8) {
-        elementsList_index[i].style.display = "none";
-        hiddenListElements_index.push(elementsList_index[i]);
-        valueHiddenElements_index.textContent = hiddenListElements_index.length;
+        elementsList[i].style.display = "none";
+        hiddenListElements.push(elementsList[i]);
+        valueHiddenElements.textContent = hiddenListElements.length;
     }
 }
 
-moreListElements_index.addEventListener('click', function() {
-    for (let i = 0; i <= hiddenListElements_index.length - 1; i ++) {
-        hiddenListElements_index[i].style.display = "block";
-        moreListElements_index.style.display = "none";
+moreListElements.addEventListener('click', function() {
+    for (let i = 0; i <= hiddenListElements.length - 1; i ++) {
+        hiddenListElements[i].style.display = "block";
+        moreListElements.style.display = "none";
     }
 })
