7ca9a2be68b9d163c0ecfc62fcfc498b2c708d84
a.merkulov <merkuller26@gmail.com>
2021-02-24 10:54:24 +0500

    Update of 'appear og footer list elements'

--- js/main.js
+++ js/main.js
@@ -880,10 +880,12 @@ let moreListElements = footerList.querySelector('.moreEl');
 let counter = 0;
 let hiddenListElements = [];
 
+moreListElements.style.visibility = 'hidden';
 
 for (let i = 0; i <= elementsList.length - 1; i ++) {
     counter ++;
     if (counter > 8) {
+        moreListElements.style.visibility = 'visible';
         elementsList[i].style.display = "none";
         hiddenListElements.push(elementsList[i]);
         valueHiddenElements.textContent = hiddenListElements.length;
