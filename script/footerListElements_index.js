9ee864f1ae6e08acad950888745830be2cb2f07e
siprov <esiprov@mottex.ru>
2021-02-25 16:53:06 +0500

    Revert "Merge remote-tracking branch 'fork/fix_73519' into fix_73519"
    This reverts commit e9f693f186b37872b49df9ec256889c231c6c1f0, reversing
    changes made to 26a45074403ea377ed822901cbf5743678298341.

--- script/footerListElements_index.js
+++ script/footerListElements_index.js
@@ -1,29 +1,27 @@
 // Этот скрипт определяет количество дополнительных элементов списка и по нажатию на кнопку "ещё" показывает их.
-// footerListElements_index
 
-let footerList = document.querySelector('.footerMenuByApplicationList');
+let footerList = document.querySelector('.footerMenuByApplicationList'); 
 let elementsList = footerList.querySelectorAll('li');
 let valueHiddenElements = footerList.querySelector('.hiddenListElements');
 let moreListElements = footerList.querySelector('.moreEl');
 let counter = 0;
 let hiddenListElements = [];
-if (moreListElements) {
-    moreListElements.style.visibility = 'hidden';
 
-    for (let i = 0; i <= elementsList.length - 1; i++) {
-        counter++;
-        if (counter > 8) {
-            moreListElements.style.visibility = 'visible';
-            elementsList[i].style.display = "none";
-            hiddenListElements.push(elementsList[i]);
-            valueHiddenElements.textContent = hiddenListElements.length;
-        }
+moreListElements.style.visibility = 'hidden';
+
+for (let i = 0; i <= elementsList.length - 1; i ++) {
+    counter ++;
+    if (counter > 8) {
+        moreListElements.style.visibility = 'visible';
+        elementsList[i].style.display = "none";
+        hiddenListElements.push(elementsList[i]);
+        valueHiddenElements.textContent = hiddenListElements.length;
     }
-
-    moreListElements.addEventListener('click', function () {
-        for (let i = 0; i <= hiddenListElements.length - 1; i++) {
-            hiddenListElements[i].style.display = "block";
-            moreListElements.style.display = "none";
-        }
-    })
 }
+
+moreListElements.addEventListener('click', function() {
+    for (let i = 0; i <= hiddenListElements.length - 1; i ++) {
+        hiddenListElements[i].style.display = "block";
+        moreListElements.style.display = "none";
+    }
+})
--- script/showPopup.js
+++ script/showPopup.js
@@ -1,22 +1,43 @@
-//showPopup.js
-//
-
-let popup = document.querySelector('.popup');
-let cards = document.querySelectorAll('.productCard');
-if (popup && cards) {
+if (document.querySelector('.productCard')) {
+    let popup = document.querySelector('.popup');
+    let cards = document.querySelectorAll('.productCard');
     let closePopupBtn = popup.querySelector('.popupCloseBtn');
-    let body = document.querySelector('body');
     let popupProductName = popup.querySelector('.popupProductName');
     let hiddenFormField = popup.querySelector('.goodName');
+    let html = document.querySelector('html');
+
+    let popupOpen = function () {
+        popup.classList.add('showPopup');
+        popup.addEventListener('click', function(e) {
+            if (!e.target.closest('.popupContent')) {
+                popupClose();
+                bodyLock();
+            }
+        })
+        bodyLock();
+    };
+
+    let popupClose = function () {
+        popup.classList.remove('showPopup');
+    };
+
+    let bodyLock = function () {
+        let popupActive = document.querySelector('.popup.showPopup');
+        if (popupActive) {
+            html.classList.add('lock');
+        } else {
+            html.classList.remove('lock');
+            
+        }
+    };
 
     let cardClickHandler = function(item) {
         let productCardBtn = item.querySelector('.refineRemainder');
         productCardBtn.addEventListener('click', function(evt) {
-            popup.classList.add('showPopup');
+            popupOpen();
             let prodcutName = item.querySelector('.productDescription > a');
             popupProductName.textContent = prodcutName.textContent;
             hiddenFormField.value = prodcutName.textContent;
-            let popupActive = document.querySelector('.popup.showPopup');
         });
     }
 
@@ -25,12 +46,14 @@ if (popup && cards) {
     };
 
     closePopupBtn.addEventListener('click', function() {
-        popup.classList.remove('showPopup');
+        popupClose();
+        bodyLock();
     });
 
     document.addEventListener('keydown', function(evt){
         if (evt.key === 'Escape') {
-            popup.classList.remove('showPopup');
+            popupClose();
+            bodyLock();
         }
     });
 }
