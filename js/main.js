a10309d9c75182e62d69606980b69b611091229b
a.merkulov <merkuller26@gmail.com>
2021-03-17 15:48:24 +0500

    Сделан калькулятор на странице productCard

--- js/main.js
+++ js/main.js
@@ -283,17 +283,34 @@ if (document.querySelector('.fieldBlock_productCard')) {
     let fieldBlock_productCard = document.querySelector('.fieldBlock_productCard');
     let widthValue = 0;
     let heightValue = 0;
-    let result = fieldBlock_productCard.querySelector('.result');
+    let result = fieldBlock_productCard.querySelector('.result_productCard');
+    let unitWidth = document.querySelector('.page3ContentHeadline').getAttribute('data-width');
+    let unitLength = document.querySelector('.page3ContentHeadline').getAttribute('data-length');
+    let quantityField = document.querySelector('.calculatorNumber_productCard');
+    let quantityLink = document.querySelector('.goodsQuantity_productCard');
+
+    // function getValue(attr) {
+    //     if (evt.target.getAttribute('name') === attr) {
+    //         return evt.target.value;
+    //     }
+    // }
 
     fieldBlock_productCard.addEventListener('input', (evt) => {
         if (evt.target.classList.contains('productCardCalcField_productCard')) {
+            // widthValue = getValue('Ширина');
+            // heightValue = getValue('Высота');
             if (evt.target.getAttribute('name') === 'Ширина') {
                 widthValue = evt.target.value;
             } else if (evt.target.getAttribute('name') === 'Высота') {
                 heightValue = evt.target.value;
             }
-            result.textContent = Number(widthValue) + Number(heightValue); 
+            result.textContent = (Math.ceil((Number(widthValue))/unitWidth)) * (Math.ceil((Number(heightValue))/unitLength)); 
         }
+    });
+
+    quantityLink.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        quantityField.value = result.textContent;
     });
 }
 // Этот скрипт переключает кнопки карточки товара и и пределяет цену за шт и метр кв в зависимости от выбранного значения толщины
