594a3380c483e787229151ccc9954a9812256957
a.merkulov <merkuller26@gmail.com>
2021-03-18 10:29:37 +0500

    Страница productCard. Апгрейд калькулятора.

--- js/main.js
+++ js/main.js
@@ -288,12 +288,33 @@ if (document.querySelector('.fieldBlock_productCard')) {
     let unitLength = document.querySelector('.page3ContentHeadline').getAttribute('data-length');
     let quantityField = document.querySelector('.calculatorNumber_productCard');
     let quantityLink = document.querySelector('.goodsQuantity_productCard');
+    let makeCalcBlock = document.querySelector('.youNeedIs_productCard');
+    let thisWord = document.querySelector('.wordToDecline_productCard');
 
     // function getValue(attr) {
     //     if (evt.target.getAttribute('name') === attr) {
     //         return evt.target.value;
     //     }
     // }
+
+    let wordArr = ['лист', 'листа', 'листов'];
+
+    function wordDecline(num, words) {
+        let count = num % 100;
+        if (count >= 5 && count <= 20) {
+            thisWord.textContent = words[2];
+        } else {
+            count = count % 10;
+            if (count === 1) {
+                thisWord.textContent = words[0];
+            } else if (count >= 2 && count <= 4) {
+                thisWord.textContent = words[1];
+            } else {
+                thisWord.textContent = words[2];
+            }
+        }
+        return thisWord.textContent;
+    }
 
     fieldBlock_productCard.addEventListener('input', (evt) => {
         if (evt.target.classList.contains('productCardCalcField_productCard')) {
@@ -304,7 +325,17 @@ if (document.querySelector('.fieldBlock_productCard')) {
             } else if (evt.target.getAttribute('name') === 'Высота') {
                 heightValue = evt.target.value;
             }
-            result.textContent = (Math.ceil((Number(widthValue))/unitWidth)) * (Math.ceil((Number(heightValue))/unitLength)); 
+            if (widthValue.length > 0 && heightValue.length > 0) {
+                makeCalcBlock.style.transform = 'translateY(25px)';
+                makeCalcBlock.style.opacity = '1';
+                result.textContent = (Math.ceil((Number(widthValue))/unitWidth)) * (Math.ceil((Number(heightValue))/unitLength)); 
+                let thisNum = Number(result.textContent);
+                wordDecline(thisNum, wordArr);
+                
+            } else {
+                makeCalcBlock.style.transform = 'translateY(0px)';
+                makeCalcBlock.style.opacity = '0';
+            }
         }
     });
 
