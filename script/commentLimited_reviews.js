993b4d95687cec022cbc802e479ada1294ec1d3d
a.merkulov <merkuller26@gmail.com>
2021-03-25 13:35:00 +0500

    Страница reviews. commentLimited_reviews обновление.

--- script/commentLimited_reviews.js
+++ script/commentLimited_reviews.js
@@ -2,7 +2,6 @@ if (document.querySelector('.contentReviews_reviews')) {
 
     let slideList_reviews = document.querySelectorAll('.s3_slide1Review_index');
     let commentBlock = document.querySelector('.contentReviews_reviews');
-    let isOpen = false;
     
     const renderBtn = (element) => {
         let btn = `<button type="button" class="readAllBtn_reviews">читать полностью</button>`;
@@ -12,18 +11,10 @@ if (document.querySelector('.contentReviews_reviews')) {
     slideList_reviews.forEach(comment => renderBtn(comment));
 
     commentBlock.addEventListener('click', function (e) {
-        isOpen = !isOpen;
         if (e.target.classList.contains('readAllBtn_reviews')) {
-            let text = e.target.parentElement.querySelector('.reviewText_index')
-            if (isOpen === true) {
-                text.style.webkitLineClamp = "none";
-                text.style.display = "block";
-                e.target.textContent = "свернуть";
-            } else {
-                text.style.webkitLineClamp = "5";
-                text.style.display = "-webkit-box";
-                e.target.textContent = "читать полностью";
-            }
+            let text = e.target.parentElement.querySelector('.reviewText_index');
+            text.classList.toggle('commentAppear');
+            e.target.textContent = text.classList.contains('commentAppear') ? 'свернуть' : 'читать полностью';
         }
     })
 }
