9a3a03e4d8f518014964d081c3d2714e882b8a7b
a.merkulov <merkuller26@gmail.com>
2021-04-02 14:56:14 +0500

    Страница Index. мм в кальке.

---
+++ indexCalcUnitOfMeasurement_index.js
@@ -0,0 +1,13 @@
+// if (document.querySelector('.filedBlock_index')) {
+
+//     let fiedlBlock = document.querySelector('.filedBlock_index');
+    
+//     fiedlBlock.addEventListener('click', (e) => {
+//         if (e.target.classList.contains('calcField_index')) {
+//             let field = e.target.parentElement;
+//             console.log(field);
+//             let x = field.document.querySelector('.valueFiled');
+//             x.style.opacity = '1';
+//         }
+//     })
+// }
