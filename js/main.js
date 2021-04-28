00a116c3f477cfaac1d3aed9472676ea31bf0403
a.merkulov <merkuller26@gmail.com>
2021-04-28 13:30:41 +0500

    Модифицирован скрипт popover_productCard

--- js/main.js
+++ js/main.js
@@ -874,13 +874,21 @@ if (document.getElementById('#phone')) {
 //popover_productCard
 if (document.querySelector('.popover_productCard')) {
 
-    function openPopup (text, type, time) {
+    function openPopup (text, type, time, link, wordLink) {
         let popover_productCard = document.querySelector('.popover_productCard');
             popoverText_productCard = document.querySelector('.popoverText_productCard');
 
+        
         popover_productCard.style.opacity = "1";
         popover_productCard.style.display = "block";
-        popoverText_productCard.textContent = text;
+        if (link && wordLink) {
+            let str = text;
+                link = `<a class="popupLink" href="${link}">${wordLink}</a>`;        
+                str = str.replace(/#word#/i, link);
+            popoverText_productCard.insertAdjacentHTML('afterbegin', str);
+        } else {
+            popoverText_productCard.textContent = text;    
+        }
         if (type === 'danger') {
             popover_productCard.style.borderColor = "#e84d4d";
             popoverText_productCard.style.color = "#e84d4d";
@@ -897,12 +905,13 @@ if (document.querySelector('.popover_productCard')) {
             popover_productCard.style.opacity = "0";
             popover_productCard.style.display = "none";
             popoverText_productCard.textContent = '';
-            popover_productCard.style.borderColor = "none";
+            popover_productCard.style.borderColor = "crimson";
             popoverText_productCard.style.color = "none";
         }, time);
     }
-
 }
+
+
 
 // Маска для поля ввода номера
 //popupPhoneNumberMask
